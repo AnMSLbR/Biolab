2ea988761e4c5f27590420ae42ca7919d0e06282
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2021-07-07 18:53:22 +0500

    Feature-8. Создание класса Experiment.

---
+++ Biolab/Experiment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biolab
+{
+    class Experiment
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Coordinate { get; set; }
+
+        public Experiment() { }
+
+        public Experiment(string name, string description, int coordinate)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Coordinate = coordinate;
+        }
+    }
+}
